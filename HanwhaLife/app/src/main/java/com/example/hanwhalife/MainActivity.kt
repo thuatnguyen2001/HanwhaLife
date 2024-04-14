9cdd7a03b3abfb960812b6ec9a2c8399d8ee6624
thuatnguyen2001 <nguyenvuthuat13102001@gmail.com>
2024-04-14 20:44:59 +0700

    fix loi

--- HanwhaLife/app/src/main/java/com/example/hanwhalife/MainActivity.kt
+++ HanwhaLife/app/src/main/java/com/example/hanwhalife/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.hanwhalife
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
