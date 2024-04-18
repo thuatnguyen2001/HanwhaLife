ab15adc0dc81c4bdc7ef56e2bddc5c866167a7a8
thuatnguyen2001 <nguyenvuthuat13102001@gmail.com>
2024-04-18 11:58:50 +0700

    toiuu

--- HanwhaLife/app/src/main/java/com/example/hanwhalife/HomeActivity.kt
+++ HanwhaLife/app/src/main/java/com/example/hanwhalife/HomeActivity.kt
@@ -3,9 +3,9 @@ package com.example.hanwhalife
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class ServiceActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_service)
+        setContentView(R.layout.activity_home)
     }
 }
